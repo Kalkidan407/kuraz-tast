8e0d35ff06a1fe9328f17cf2792efb690b250b0a
Kalkidan407 <kelemework267@gmail.com>
2025-06-05 07:13:19 +0100

    finished

--- kuraztest/lib/main.dart
+++ kuraztest/lib/main.dart
@@ -1,19 +1,114 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ToDoApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class ToDoApp extends StatelessWidget {
+  const ToDoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      home: TaskListScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Task {
+  String title;
+  bool isDone;
+
+  Task(this.title, {this.isDone = false});
+}
+
+class TaskListScreen extends StatefulWidget {
+  @override
+  State<TaskListScreen> createState() => _TaskListScreenState();
+}
+
+class _TaskListScreenState extends State<TaskListScreen> {
+  final List<Task> tasks = [];
+  final TextEditingController _controller = TextEditingController();
+
+  void addTask(String title) {
+    setState(() {
+      tasks.add(Task(title));
+      _controller.clear();
+    });
+  }
+
+  void toggleTask(int index) {
+    setState(() {
+      tasks[index].isDone = !tasks[index].isDone;
+    });
+  }
+
+  void deleteTask(int index) {
+    setState(() {
+      tasks.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('To-Do List')),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: const InputDecoration(
+                      labelText: 'Add a task',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: () {
+                    if (_controller.text.trim().isNotEmpty) {
+                      addTask(_controller.text.trim());
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: (_, index) {
+                final task = tasks[index];
+                return ListTile(
+                  title: Text(
+                    task.title,
+                    style: TextStyle(
+                      decoration:
+                          task.isDone
+                              ? TextDecoration.lineThrough
+                              : TextDecoration.none,
+                    ),
+                  ),
+                  leading: Checkbox(
+                    value: task.isDone,
+                    onChanged: (_) => toggleTask(index),
+                  ),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () => deleteTask(index),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
